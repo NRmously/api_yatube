34da0184fa115c91a8cd83a7b9dbbbcdf3055c26
Andrey Mirt <nrmously@gmail.com>
2024-08-09 12:20:26 +0300

    First commit.

---
+++ yatube_api/api/views.py
@@ -0,0 +1,44 @@
+from rest_framework.generics import get_object_or_404
+from rest_framework import viewsets, permissions
+
+from .serializers import GroupSerializer, PostSerializer, CommentSerializer
+from posts.models import Group, Post
+
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
+
+
+API_PERMISSIONS = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    permission_classes = API_PERMISSIONS
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    permission_classes = API_PERMISSIONS
+    serializer_class = CommentSerializer
+
+    def get_post(self):
+        return get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+
+    def get_queryset(self):
+        return self.get_post().comments.all()
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user, post=self.get_post())
